3d859e085789827e571ead3ae4ccbd7419996a9e
Dale <583652+TuxedoHam@users.noreply.github.com>
2025-05-10 23:00:22 +0930

    Create config.php
    fix to help the setup.php to work correctly for config details to be entered and saved

---
+++ config/config.php
@@ -0,0 +1,21 @@
+<?php
+# This is an auto-generated config-file!
+# Be careful, when manual editing this!
+
+date_default_timezone_set('UTC');
+define("NXDNREFLECTORNAME", "NXDN Reflector");
+define("NXDNREFLECTORLOGPATH", "/var/log/NXDNReflector/");
+define("NXDNREFLECTORLOGPREFIX", "NXDNReflector");
+define("NXDNREFLECTORINIPATH", "/etc/");
+define("NXDNREFLECTORINIFILENAME", "NXDNReflector.ini");
+define("NXDNREFLECTORPATH", "/usr/local/bin/");
+define("TIMEZONE", "Australia/Melbourne");
+define("LOGO", "nxdn.jpg");
+define("REFRESHAFTER", "120");
+define("SHOWPROGRESSBARS", "on");
+define("SHOWDISK", "on");
+define("SHOWSYSTEM", "on");
+define("TEMPERATUREALERT", "on");
+define("TEMPERATUREHIGHLEVEL", "40");
+define("SHOWQRZ", "on");
+?>
